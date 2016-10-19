b9fdad5147cfd5d8d9589c0221496215b58a83da
nbarton915 <nrbarton@studentbody.byu.edu>
2016-10-18 19:54:41 -0600

    Up to Checkpoint 2

---
+++ BlowOut/BlowOut/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BlowOut
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "email",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Email", name = UrlParameter.Optional, emai = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ BlowOut/BlowOut/Controllers/ContactController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public String Index()
+        {
+            return "Please call support at <b><u>801-555-1212</u></b>. Thank you!";
+        }
+
+        // email
+        public String Email(String name, String email)
+        {
+            return "Thank you " + name + " We will send an email to " + email;
+        }
+    }
+}
