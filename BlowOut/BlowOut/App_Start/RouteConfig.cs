f2e03651731588dc6de2771663363c7d807b0530
nbarton915 <nrbarton@studentbody.byu.edu>
2016-10-19 13:48:04 -0600

    Added Rentals View and link on the BlowOut view. Updated the route config and html helpers on the layout navbar to allow the BlowOut navbar link so that it routes properly

--- BlowOut/BlowOut/App_Start/RouteConfig.cs
+++ BlowOut/BlowOut/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BlowOut
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "BlowOut", id = UrlParameter.Optional }
             );
 
             routes.MapRoute(
--- BlowOut/BlowOut/Controllers/HomeController.cs
+++ BlowOut/BlowOut/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace BlowOut.Controllers
             return View();
         }
 
+        public ActionResult Rentals()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
