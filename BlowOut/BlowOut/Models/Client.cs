d9d6e32d0e8a580f1318197556ad377c177f9930
nbarton915 <nrbarton@studentbody.byu.edu>
2016-10-31 18:32:12 -0600

    Created Database, added connection string to web.config, created the client and instrument models

---
+++ BlowOut/BlowOut/Models/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BlowOut.Models
+{
+    [Table("Client")]
+    public class Client
+    {
+        [Key]
+        public int clientID { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zip { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+    }
+}
---
+++ BlowOut/BlowOut/Models/Instrument.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BlowOut.Models
+{
+    [Table("Instrument")]
+    public class Instrument
+    {
+        [Key]
+        public int instrumentID { get; set; }
+        public string description { get; set; }
+        public string type { get; set; }
+        public double price { get; set; }
+        public int clientID { get; set; }
+    }
+}
