98ef6235ec6a75b7dabe4f10654839df4e73e955
nbarton915 <nrbarton@studentbody.byu.edu>
2016-11-02 16:29:17 -0600

    Created AddClient Model with fields

---
+++ BlowOut/BlowOut/Models/AddClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlowOut.Models
+{
+    public class AddClient
+    {
+        [Key]
+        [ReadOnly(true)]
+        public int clientID { get; set; }
+
+        [Required]
+        [DisplayName("First Name")]
+        public string firstname { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
+        public string lastname { get; set; }
+
+        [Required]
+        [DisplayName("Address")]
+        public string address { get; set; }
+
+        [Required]
+        [DisplayName("City")]
+        public string city { get; set; }
+
+        [Required]
+        [StringLength(2)]
+        [DisplayName("City")]
+        public string state { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[0-9]{5}$")]
+        [DisplayName("Zip")]
+        public int zip { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DisplayName("Email")]
+        public string email { get; set; }
+
+        [Required]
+        [RegularExpression("@^([0-9]{3})-[0-9]{3}-[0-9]{4}")]
+        [DisplayName("Phone Number")]
+        public string phone { get; set; }
+
+        [Key]
+        public int instrumentID { get; set; }
+        public string description { get; set; }
+        public string type { get; set; }
+        public double price { get; set; }
+        public int clientID { get; set; }
+    }
+}
